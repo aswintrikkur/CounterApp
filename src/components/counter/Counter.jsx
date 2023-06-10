b768b7df32f3c4adafb1f2b135f0edf84bb31d1b
ASWINKUMAR C M <aswin.trikkur@gmail.com>
2023-06-10 17:46:06 +0530

    loader bar added

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -2,18 +2,38 @@ import { useState } from 'react'
 import './Counter.css'
 
 
+
 export const Counter = () => {
     const [count, setCount] = useState(0);
+    const [div, setDiv] = useState([]);
+
+    console.log("count: ", count);
+    console.log('div length: ', div.length);
 
     const handleDecrement = () => {
         { count > 0 && setCount(prev => (prev - 1)); }
+
+        // div.pop(); //this won't work
+        // setDiv(div);
+
+        // setDiv(prev=>(prev.pop()));  //this too won't work
+
+        const newDiv = [...div];
+        newDiv.pop();
+        setDiv(newDiv);
     }
     const handleIncrement = () => {
-        setCount(prev => (prev + 1));
+        if (count < 20) {
+            setCount(prev => (prev + 1))
+            { (count >= 1 && count < 20) && setDiv([...div, <div className="content"></div>]) }
+        };
     }
     const handleReset = () => {
-        setCount(prev => (0));
+        setCount(0);
+        setDiv([]);
     }
+
+
 
     return (
         <div>
@@ -25,8 +45,10 @@ export const Counter = () => {
                     <button onClick={handleIncrement}>+</button>
                 </div>
                 <div className="footer">
-                    <div className="body">
-
+                    <div className="body"  >
+                        {count >= 1 && <div className="content content1" ></div>}
+                        {div}
+                        {count == 20 && <div className="content content-last" ></div>}
                     </div>
                 </div>
             </div>
